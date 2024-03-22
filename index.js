d7f3bb6c72a5fd172ef84b7d7e14b632d61cab71
M.Ibad khan <160883583+IbadKhan213@users.noreply.github.com>
2024-03-23 01:01:32 +0500

    created number guessing gamw

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNum = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userInput",
+        message: "Please guess a number between 1 to 10 : ",
+    }
+]);
+if (answer.userInput === randomNum) {
+    console.log("Congratulations! you guessed right number");
+}
+else {
+    console.log("You guessed wrong number");
+}
